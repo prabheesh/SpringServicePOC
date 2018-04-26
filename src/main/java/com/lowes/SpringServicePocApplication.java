93625fdf442aa1217c412ed66c67986cfd6f5421
Chandran <prachand@lowes.com>
2018-04-26 19:24:58 +0530

    first commit

---
+++ src/main/java/com/lowes/SpringServicePocApplication.java
@@ -0,0 +1,12 @@
+package com.lowes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServicePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServicePocApplication.class, args);
+	}
+}
